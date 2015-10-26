c4b8579dbef153bd0e07fb1d5799129596201a68
Chris Whong <chris@whong.org>
2015-10-25 23:06:36 -0500

    Adding route param so you can use any bus route

--- app.js
+++ app.js
@@ -6,15 +6,6 @@ var app = express();
 //B52 Bus working api call
 //http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?&LineRef=MTA%20NYCT_B52&key=adf3b381-85b5-48b9-a049-32c335108f6e
 
-var options = {
-  route: 'B52',
-  apiKey: 'adf3b381-85b5-48b9-a049-32c335108f6e'
-}
-
-var template = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?&LineRef=MTA%20NYCT_{{route}}&key={{apiKey}}'
-
-var apiCall = Mustache.render(template,options);
-
 //empty geojson featureCollection
 var featureCollection = {
   type: 'FeatureCollection',
@@ -22,21 +13,36 @@ var featureCollection = {
 }
 
 //create a route for a get request to /api/b52
-app.get('/api/b52', function(req, res) {
+app.get('/api/:route', function(req, res) {
+  var options = {
+    route: req.params.route.toUpperCase(),
+    apiKey: 'adf3b381-85b5-48b9-a049-32c335108f6e'
+  }
+
+  var template = 'http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?&LineRef=MTA%20NYCT_{{route}}&key={{apiKey}}'
+
+  var apiCall = Mustache.render(template,options);
+
+  console.log('Getting data for ',apiCall);
+
   //get data from bustime
   request(apiCall, function(err,response,body) {
     var data = JSON.parse(body);
-    var vehicles = data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0].VehicleActivity;
+    data = data.Siri.ServiceDelivery.VehicleMonitoringDelivery[0];
+    if(data.VehicleActivity) {
+      var vehicles = data.VehicleActivity;
 
-    //iterate over vehicles, convert each to a geojson feature
-    vehicles.forEach(function(vehicle) {
-      var feature = makeFeature(vehicle);
-      featureCollection.features.push(feature);
-    });
+      //iterate over vehicles, convert each to a geojson feature
+      vehicles.forEach(function(vehicle) {
+        var feature = makeFeature(vehicle);
+        featureCollection.features.push(feature);
+      });
 
-    //send the featureCollection as the response
-    res.send(JSON.stringify(featureCollection));
-
+      //send the featureCollection as the response
+      res.send(JSON.stringify(featureCollection));
+    } else {
+      res.send('Something went wrong');
+    }
   });
 });
 
